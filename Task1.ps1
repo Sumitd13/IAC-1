328e2c33d45d75c28057d1bdf6ed080c21aa9345
Sumitd13 <44285826+Sumitd13@users.noreply.github.com>
2018-10-20 16:16:53 +0530

    Add files via upload

---
+++ Task1.ps1
@@ -0,0 +1,2 @@
+﻿Set-Content -Path 'C:\Users\Administrator\Desktop\Task_201018\SUMIT\Test.txt' -Value "Hello World Test"
+Get-Content -Path 'C:\Users\Administrator\Desktop\Task_201018\SUMIT\Test.txt'
